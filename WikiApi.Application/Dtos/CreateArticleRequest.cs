31d1a291e5f3d9c7b54bd32ce643d93ab183db13
Marcos Aurelio <marcos.aucorrea@gmail.com>
2026-05-10 11:01:11 -0300

    Validacoes Dtos

--- WikiApi.Application/Dtos/CreateArticleRequest.cs
+++ WikiApi.Application/Dtos/CreateArticleRequest.cs
@@ -1,3 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WikiApi.Application.Dtos;
 
-public record CreateArticleRequest(string Title, string Content, string Tags, string Category);
+public record CreateArticleRequest(
+    [Required, StringLength(100)] string Title,
+    [Required, StringLength(5000)] string Content,
+    [StringLength(200)] string Tags,
+    [Required, StringLength(50)] string Category
+);
--- WikiApi.Application/Dtos/UpdateArticleRequest.cs
+++ WikiApi.Application/Dtos/UpdateArticleRequest.cs
@@ -1,3 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WikiApi.Application.Dtos;
 
-public record UpdateArticleRequest(int Id, string Title, string Content, string Tags, string Category);
+public record UpdateArticleRequest(
+    [Required] int Id,
+    [Required, StringLength(100)] string Title,
+    [Required, StringLength(5000)] string Content,
+    [StringLength(200)] string Tags,
+    [Required, StringLength(50)] string Category
+);
